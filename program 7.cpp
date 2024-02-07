367e02ebb6da9a99a03e1d452ef8ee213ca0ae23
Prav8797 <113341132+Prav8797@users.noreply.github.com>
2024-02-07 21:05:52 +0530

    Add files via upload

---
+++ program 7.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter a number ";
+    cin>>n;
+    if(n>0)
+    {
+        cout<<"Positive number";
+    }
+    else if(n<0)
+    {
+        cout<<"Negative number";
+    }
+    else
+    {
+        cout<<"Zero";
+    }
+    return 0;
+}
---
+++ program 9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+float num= 11.66;
+float var = -3.6;
+cout<<"floor value of the num is "<<floor(num)<<endl;
+cout<<"floor value of the var is "<<floor(var)<<endl;
+cout<<" Ceil value of the num is "<<ceil(num)<<endl;
+cout<<" Ceil value of the var is "<<ceil(var)<<endl;
+return 0;
+}
